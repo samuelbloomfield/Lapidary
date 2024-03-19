8a022ec487aeadd591303ad6e277964b26c4c8c7
samuelbloomfield <bloomy324@hotmail.com>
2024-03-19 22:27:31 +0000

    Update Lapidary.pl
    Fixed read alignment to take into account gaps and terminal codons

--- Lapidary.pl
+++ Lapidary.pl
@@ -1,7 +1,7 @@
 ###
 #	Lapidary.pl
 #	Script to determine the read coverage and identity to a protein database using diamond
-#	Last modified: 10/11/2023
+#	Last modified: 19/03/2024
 #	Author: Samuel Bloomfield
 ###
 
@@ -30,8 +30,8 @@ my $read_1;
 my $read_2="";
 my $db;
 my $threads = 1; #Default to 1 thread
-my $identity = 80; #Default to 80% identity
-my $coverage = 20; #Default to 20% coverage
+my $identity = 70; #Default to 70% identity
+my $coverage = 50; #Default to 50% coverage
 my $read_type;
 my $help;
 my $version;
@@ -62,8 +62,8 @@ if(defined($help)){
 	read_2\tLocation of second read file if read files are paired\n
 	db\tFull location to fasta file containing amino acid sequences (required)\n
 	threads\tNumber of threads to use for Diamond (default: 1)\n
-	identity\tDiamond identity percentage cut-off to use (default: 80)\n
-	coverage\tDiamond coverage percentage cut-off to use (default: 20)\n
+	identity\tDiamond identity percentage cut-off to use (default: 70)\n
+	coverage\tDiamond coverage percentage cut-off to use (default: 50)\n
 	read_type\tTypes of reads used (required): single or paired\n
 	sequence_identification\tMethod for calling most likely sequence: identity (default) or consensus\n
 	help\tDisplay help screen\n
@@ -72,7 +72,7 @@ if(defined($help)){
 
 #Print out version
 if(defined($version)){
-	die "\n\nLapidary version 0.4.0\n\n";
+	die "\n\nLapidary version 0.5.0\n\n";
 } 
 
 
@@ -221,10 +221,19 @@ my $read_identity;
 my @sample_identity;
 my @protein_identity;
 my @consensus_identity;
+my @sample_length;
 
 my @identity_index;
 
-  
+my $adjusted_identity;
+my $adjusted_coverage;
+my @adjusted_amino_acids;
+
+my $partial_match;
+my $partial_count;
+my $partial_position_current;  
+
+my $temp;
 
 #Use first read file to name outputs
 if ($read_1=~m/^.+\/(.*?)\.f.*?$/) { 
@@ -252,6 +261,7 @@ if ($read_1=~m/^.+\/(.*?)\.f.*?$/) {
 	@sample_positions = ();
 	@sample_amino_acids = ();
 	@sample_identity = ();
+	@sample_length = ();
 
 	open DIAMOND, $diamond_output or die "File $diamond_output NOT FOUND - $!\n";
 	foreach $_ (<DIAMOND>) {
@@ -293,6 +303,8 @@ if ($read_1=~m/^.+\/(.*?)\.f.*?$/) {
 			push @sample_positions, $read_position;
 			push @sample_amino_acids, $read_translate;	
 			push @sample_identity, $read_identity;
+			push @sample_length, $read_length;
+
 			}
 		}
 	}
@@ -305,9 +317,9 @@ if ($read_1=~m/^.+\/(.*?)\.f.*?$/) {
 
 	for (my $n=0; $n < scalar(@unique_proteins); $n++) {
 		for (my $o=0; $o < scalar(@protein_names); $o++) {
-		if($unique_proteins[$n] eq $protein_names[$o]) {
-			push @unique_sequences, $protein_sequences[$o];
-		}
+			if($unique_proteins[$n] eq $protein_names[$o]) {
+				push @unique_sequences, $protein_sequences[$o];
+			}
 		}
 	}
 		
@@ -336,16 +348,67 @@ if ($read_1=~m/^.+\/(.*?)\.f.*?$/) {
 
 				@amino_acids_split = split(//, $sample_amino_acids[$l]);
 				
-				$sample_position_current = $sample_positions[$l];
-
-				for (my $i=0; $i < scalar(@amino_acids_split); $i++) {
-
-#				push @protein_reads, $sample_reads[$l];
-				push @protein_positions, $sample_position_current;
-				push @protein_amino_acids, $amino_acids_split[$i];
-				push @protein_identity, $sample_identity[$l];
+				@adjusted_amino_acids=();
+								
+				#Loop through alignment and adjust if interupted
+				$partial_match = 0;
+				$partial_count = 0;
+				$partial_position_current =  $sample_positions[$l];
 				
-				$sample_position_current++;
+				#Trim read alignments if there is stop codon
+				for (my $z=0; $z < scalar(@amino_acids_split); $z++) {
+					#Check if read amino acid position is within reference amino acid alignment
+					if(length($unique_sequences[$j]) < $partial_position_current ) {
+					} else {
+						if($amino_acids_split[$z] eq "*"){
+							push(@adjusted_amino_acids, $amino_acids_split[$z]);
+							$partial_count++;
+							
+							if($amino_acids_split[$z] eq (substr($unique_sequences[$j],$partial_position_current, 1))){
+								$partial_match++;
+							}
+							$partial_position_current++;
+							last;
+						}elsif($amino_acids_split[$z] eq "X"){
+							push(@adjusted_amino_acids, $amino_acids_split[$z]);
+							$partial_count++;
+							
+							if($amino_acids_split[$z] eq (substr($unique_sequences[$j],$partial_position_current, 1))){
+								$partial_match++;
+							}
+							$partial_position_current++;
+							last;
+						} else {
+							push(@adjusted_amino_acids, $amino_acids_split[$z]);
+							$partial_count++;
+
+							if($amino_acids_split[$z] eq (substr($unique_sequences[$j],$partial_position_current, 1))){
+								$partial_match++;
+							}
+							$partial_position_current++;
+						}
+					}
+				}
+				
+				#Adjust read coverage and identity
+				$adjusted_identity = ($partial_match / $partial_count) * 100;
+				
+				$adjusted_coverage = (($partial_count * 3)/ $sample_length[$l]) * 100;
+				
+				#Extract amino acids from reads that pass QC
+				if($adjusted_identity >= $identity) {
+					if($adjusted_coverage >= $coverage) {
+						$sample_position_current = $sample_positions[$l];
+						
+						for (my $i=0; $i < scalar(@adjusted_amino_acids); $i++) {
+#							push @protein_reads, $sample_reads[$l];
+							push @protein_positions, $sample_position_current;
+							push @protein_amino_acids, $adjusted_amino_acids[$i];
+							push @protein_identity, $adjusted_identity;
+						
+							$sample_position_current++;
+						}
+					}
 				}
 			}
 		}
@@ -383,20 +446,20 @@ if ($read_1=~m/^.+\/(.*?)\.f.*?$/) {
 		
 		for (my $k=0; $k < scalar(@protein_split); $k++) {
 
-		$position_depth = 0;
+			$position_depth = 0;
 
-		@indexes = ();					
+			@indexes = ();					
 		
-		$alignment_end = $k;
+			$alignment_end = $k;
 
-		@consensus = ();
-		@consensus_identity = ();
+			@consensus = ();
+			@consensus_identity = ();
 		
-		#Loop through each protein position
-		for (my $p=0; $p < scalar(@protein_positions); $p++) {	
+			#Loop through each protein position
+			for (my $p=0; $p < scalar(@protein_positions); $p++) {	
 				
-			#Check if positions match
-			if($protein_positions[$p] == $k){
+				#Check if positions match
+				if($protein_positions[$p] == $k){
 				push @indexes, $p;
 
 				$depth++;
@@ -405,86 +468,82 @@ if ($read_1=~m/^.+\/(.*?)\.f.*?$/) {
 				#Store amino acid at position for consensus
 				push(@consensus, $protein_amino_acids[$p]);
 				push(@consensus_identity, $protein_identity[$p]);
+				
+				}
 			}
-		}
 
-		if($sequence_identification eq "consensus") {
-			#Determine most common amino acid for position
-			if (scalar(@consensus) > 0) {
-				@uniq_consensus_count = ();
+			if($sequence_identification eq "consensus") {
+				#Determine most common amino acid for position
+				if (scalar(@consensus) > 0) {
+					@uniq_consensus_count = ();
 				
-				@uniq_consensus = uniq(@consensus);
+					@uniq_consensus = uniq(@consensus);
 				
-				for (my $p=0; $p < (scalar(@uniq_consensus)); $p++) {
+					for (my $p=0; $p < (scalar(@uniq_consensus)); $p++) {
 
-					$variant_count = 0;
+						$variant_count = 0;
 
-					for (my $q=0; $q < (scalar(@consensus)); $q++) {
-						if($uniq_consensus[$p] eq $consensus[$q]) {
-							$variant_count++;
+						for (my $q=0; $q < (scalar(@consensus)); $q++) {
+							if($uniq_consensus[$p] eq $consensus[$q]) {
+								$variant_count++;
+							}
 						}
+
+						push(@uniq_consensus_count, $variant_count);
 					}
 
-					push(@uniq_consensus_count, $variant_count);
-				}
+					#Sort count from largest to smallest
+					my @sorted_uniq_consensus_count = sort { $uniq_consensus_count[$b] <=> $uniq_consensus_count[$a] } 0..$#uniq_consensus_count;
 
-				#Sort count from largest to smallest
-				my @sorted_uniq_consensus_count = sort { $uniq_consensus_count[$b] <=> $uniq_consensus_count[$a] } 0..$#uniq_consensus_count;
-
-				@uniq_consensus = @uniq_consensus[@sorted_uniq_consensus_count];
+					@uniq_consensus = @uniq_consensus[@sorted_uniq_consensus_count];
 			
-				push (@consensus_positions, $uniq_consensus[0]);
+					push (@consensus_positions, $uniq_consensus[0]);
 
-				#Determine if consensus and reference position match
-				if($protein_split[$k] eq $uniq_consensus[0]){
-					$match++;
-				}	
-			}
-		} elsif ($sequence_identification eq "identity") {
+					#Determine if consensus and reference position match
+					if($protein_split[$k] eq $uniq_consensus[0]){
+						$match++;
+					}	
+				}
+			} elsif ($sequence_identification eq "identity") {
 
-			#Determine amino acid for position based on the best identity for the read
-			if (scalar(@consensus) > 0) {
+				#Determine amino acid for position based on the best identity for the read
+				if (scalar(@consensus) > 0) {
 
-				#Sort read positions by read identity
-				@identity_index = sort { $consensus_identity[$b] <=> $consensus_identity[$a] } 0 .. $#consensus_identity;
+					#Sort read positions by read identity
+					@identity_index = sort { $consensus_identity[$b] <=> $consensus_identity[$a] } 0 .. $#consensus_identity;
 
-				@consensus_identity = @consensus_identity[@identity_index];
-				@consensus = @consensus[@identity_index];
+					@consensus_identity = @consensus_identity[@identity_index];
+					@consensus = @consensus[@identity_index];
 
-				push (@consensus_positions, $consensus[0]);
+					push (@consensus_positions, $consensus[0]);
 				
-				#Determine if consensus and reference position match
-				if($protein_split[$k] eq $consensus[0]){
-					$match++;
-				}	
-
+					#Determine if consensus and reference position match
+					if($protein_split[$k] eq $consensus[0]){
+						$match++;
+					}	
+				}
+			} else {
+				if (scalar(@consensus) > 0) {
+					#If unknown consensus method is given, leave "-" as consensus position
+					push (@consensus_positions, "-");
+				}
 			}
-		} else {
-			if (scalar(@consensus) > 0) {
-			#If unknown consensus method is given, leave "-" as consensus position
-			push (@consensus_positions, "-");
-			}
-		}
 				
-		#Remove matched positions
-		if(scalar(@indexes) > 0) {
-			my @index_sorted = sort { $b <=> $a } @indexes;
+			#Remove matched positions
+			if(scalar(@indexes) > 0) {
+				my @index_sorted = sort { $b <=> $a } @indexes;
 
-			for (my $m=0; $m < (scalar(@index_sorted)); $m++) {
-#				splice @protein_reads, $index_sorted[$m], 1;
-				splice @protein_positions, $index_sorted[$m], 1;
-				splice @protein_amino_acids, $index_sorted[$m], 1;
-				splice @protein_identity, $index_sorted[$m], 1;
+				for (my $m=0; $m < (scalar(@index_sorted)); $m++) {
+#					splice @protein_reads, $index_sorted[$m], 1;
+					splice @protein_positions, $index_sorted[$m], 1;
+					splice @protein_amino_acids, $index_sorted[$m], 1;
+					splice @protein_identity, $index_sorted[$m], 1;
 				}
 			}
 		
-			if($position_depth > 0) {
-				$cover++;
-				if($k < $alignment_start) {
-					$alignment_start = $k;
-				}
-
-			} else {
+			
+		
+			if($position_depth == 0) {
 				push @position_match_array, $match;
 				push @position_cover_array, $cover;
 				push @position_depth_array, $depth;
@@ -499,6 +558,41 @@ if ($read_1=~m/^.+\/(.*?)\.f.*?$/) {
 				@consensus_positions=();
 				
 				$alignment_start = 1000000000000000000;
+			} elsif($consensus_positions[-1] eq "*") {
+				push @position_match_array, $match;
+				push @position_cover_array, $cover;
+				push @position_depth_array, $depth;
+				push @position_start_array, ($alignment_start + 1);
+				push @position_end_array, $alignment_end;
+				push @consensus_chunks, join("", @consensus_positions);
+					
+				$match = 0;
+				$cover = 0;
+				$depth = 0;
+				
+				@consensus_positions=();
+				
+				$alignment_start = 1000000000000000000;
+			} elsif($consensus_positions[-1] eq "X") {
+				push @position_match_array, $match;
+				push @position_cover_array, $cover;
+				push @position_depth_array, $depth;
+				push @position_start_array, ($alignment_start + 1);
+				push @position_end_array, $alignment_end;
+				push @consensus_chunks, join("", @consensus_positions);
+					
+				$match = 0;
+				$cover = 0;
+				$depth = 0;
+				
+				@consensus_positions=();
+				
+				$alignment_start = 1000000000000000000;
+			} else {
+				$cover++;
+				if($k < $alignment_start) {
+					$alignment_start = $k;
+				}
 			}
 		}
 
@@ -528,9 +622,10 @@ if ($read_1=~m/^.+\/(.*?)\.f.*?$/) {
 
 		$coverage_proportion = $position_cover_array[0] / scalar(@protein_split);
 		$mean_read_depth = $position_depth_array[0] / scalar(@protein_split);
-
-		print OUT "$unique_proteins[$j]\t$coverage_proportion\t$identity_proportion\t$mean_read_depth\t$position_start_array[0]\t$position_end_array[0]\t$consensus_chunks[0]\n";
-
+		
+		if($coverage_proportion > 0){
+			print OUT "$unique_proteins[$j]\t$coverage_proportion\t$identity_proportion\t$mean_read_depth\t$position_start_array[0]\t$position_end_array[0]\t$consensus_chunks[0]\n";
+		}
 	}
 		
 	close OUT;
